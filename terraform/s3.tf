2e9adbd88567e310bdadfefb816cf07272b5c3d0
tamanh-19127330 <letamanhlol@gmail.com>
2023-10-11 11:34:44 +0700

    test terraform deploy resource fix 5

--- terraform/s3.tf
+++ terraform/s3.tf
@@ -8,7 +8,7 @@ resource "aws_s3_bucket" "example" {
 }
 
 
-resource "aws_s3_bucket_ownership_controls" "s3_bucket_acl_ownership" {
+resource "aws_s3_bucket_ownership_controls" "example" {
   bucket = aws_s3_bucket.example.id
   rule {
     object_ownership = "BucketOwnerPreferred"
