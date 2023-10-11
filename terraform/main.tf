33dd14309eb955a3f927d609877b86141ae779e4
tamanh-19127330 <letamanhlol@gmail.com>
2023-10-11 10:33:24 +0700

    test terraform deploy resource

--- terraform/main.tf
+++ terraform/main.tf
@@ -9,19 +9,12 @@ terraform {
   backend "s3" {
     bucket = "mtr050-test"
     key    = "terraform/tf.state"
-    region = "us-east-1"
+    region = "terraform-test-github"
   }
 }
 
-variable "aws_access_key"{
-default = "AKIAXALHWFOGYYHSNF5L"
-}
 
-variable "aws_secret_key"{
-default = "eMZsbC8pAq5cZiTY01syQ/Fx3NGOP3647pO9ThDe"
-}
 
 provider "aws" {
-access_key=var.aws_access_key
-secret_key=var.aws_secret_key
+
 }
