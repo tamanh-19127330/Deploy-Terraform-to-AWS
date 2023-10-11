2e2a1ad11dfd0e30fb4954056c0359e57fa2e339
tamanh-19127330 <letamanhlol@gmail.com>
2023-10-11 15:13:42 +0700

    test terraform deploy resource fix 5

--- terraform/s3.tf
+++ terraform/s3.tf
@@ -7,21 +7,13 @@ resource "aws_s3_bucket" "example" {
   }
 }
 
-resource "aws_s3_bucket_public_access_block" "example" {
-  bucket = aws_s3_bucket.example.id
-
-  block_public_acls       = false
-  block_public_policy     = false
-  ignore_public_acls      = false
-  restrict_public_buckets = false
-}
 
 resource "aws_s3_bucket_ownership_controls" "example" {
   bucket = aws_s3_bucket.example.id
+  
   rule {
     object_ownership = "BucketOwnerPreferred"
   }
-  depends_on = [aws_s3_bucket_public_access_block.example]
 }
 
 resource "aws_s3_bucket_acl" "example" {
